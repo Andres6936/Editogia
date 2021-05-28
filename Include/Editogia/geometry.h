15e572d46a18214d5dd15c73a417dff470102137
Joan Andrés <andres6936@live.com>
2021-05-28 13:18:40 -0500

    Deleted unused methods

--- Include/Editogia/geometry.h
+++ Include/Editogia/geometry.h
@@ -38,10 +38,6 @@ enum Direction_full
 	DIRFULL_NORTHWEST
 };
 
-std::string Direction_name(Direction dir);
-
-std::string Direction_name(Direction_full dir);
-
 struct Point
 {
 	int x;
@@ -92,103 +88,6 @@ inline Point operator+(Point lhs, const Point& rhs)
 	return lhs;
 }
 
-struct Tripoint
-{
-	int x;
-	int y;
-	int z;
-
-	Tripoint(int X = 0, int Y = 0, int Z = 0) : x(X), y(Y), z(Z)
-	{
-	}
-
-	Tripoint(const Tripoint& p) : x(p.x), y(p.y), z(p.z)
-	{
-	}
-
-	~Tripoint()
-	{
-	}
-
-	std::string str();
-
-	bool operator==(const Tripoint& other) const
-	{
-		return (x == other.x && y == other.y && z == other.z);
-	}
-
-	bool operator!=(const Tripoint& other) const
-	{
-		return !(*this == other);
-	}
-
-	Tripoint& operator+=(const Tripoint& rhs)
-	{
-		x += rhs.x;
-		y += rhs.y;
-		z += rhs.z;
-
-		return *this;
-	}
-
-	Tripoint& operator+=(const Point& rhs)
-	{
-		x += rhs.x;
-		y += rhs.y;
-
-		return *this;
-	}
-
-	Tripoint& operator-=(const Tripoint& rhs)
-	{
-		x -= rhs.x;
-		y -= rhs.y;
-		z -= rhs.z;
-
-		return *this;
-	}
-
-	Tripoint& operator-=(const Point& rhs)
-	{
-		x -= rhs.x;
-		y -= rhs.y;
-
-		return *this;
-	}
-
-	operator Point()
-	{
-		Point ret;
-		ret.x = x;
-		ret.y = y;
-		return ret;
-	}
-};
-
-inline Tripoint operator+(Tripoint lhs, const Tripoint& rhs)
-{
-	lhs += rhs;
-	return lhs;
-}
-
-inline Tripoint operator+(Tripoint lhs, const Point& rhs)
-{
-	lhs += rhs;
-	return lhs;
-}
-
-inline Tripoint operator-(Tripoint lhs, const Tripoint& rhs)
-{
-	lhs -= rhs;
-	return lhs;
-}
-
-inline Tripoint operator-(Tripoint lhs, const Point& rhs)
-{
-	lhs -= rhs;
-	return lhs;
-}
-
 struct Pointcomp
 {
 	bool operator()(const Point& lhs, const Point& rhs) const
@@ -201,35 +100,13 @@ struct Pointcomp
 	}
 };
 
-struct Tripointcomp
-{
-	bool operator()(const Tripoint& lhs, const Tripoint& rhs) const
-	{
-		if (lhs.x < rhs.x) return true;
-		if (lhs.x > rhs.x) return false;
-		if (lhs.y < rhs.y) return true;
-		if (lhs.y > rhs.y) return false;
-		if (lhs.z < rhs.z) return true;
-		if (lhs.z > rhs.z) return false;
-		return false;
-	}
-};
-
 std::vector<Point> line_to(int x0, int y0, int x1, int y1);
 
 std::vector<Point> line_to(Point origin, Point target);
 
-std::vector<Tripoint> line_to(Tripoint origin, Tripoint target);
-
-std::vector<Tripoint> line_to(int x0, int y0, int z0, int x1, int y1, int z1);
-
 int rl_dist(int x0, int y0, int x1, int y1);
 
 int rl_dist(Point origin, Point target);
-
-int rl_dist(int x0, int y0, int z0, int x1, int y1, int z1);
-
-int rl_dist(Tripoint origin, Tripoint target);
 
 // sqrt(dX^2 + dY^2)
 int trig_dist(int x0, int y0, int x1, int y1);
@@ -240,25 +117,6 @@ int trig_dist(Point origin, Point target);
 int manhattan_dist(int x0, int y0, int x1, int y1);
 
 int manhattan_dist(Point origin, Point target);
-
-int manhattan_dist(int x0, int y0, int z0, int x1, int y1, int z1);
-
-int manhattan_dist(Tripoint origin, Tripoint target);
-
-/* Direction origin moves to reach target
- * This is GENERAL direction, which means that if target is 500 tiles to the
- * north and 5 tiles to the west, we return north since any reasonable
- * observe would call that the "north" and not the "northwest."
- * Generally, if dY >= 2 * dX then treat dX as 0, etc.
- * All four are functionally equivalent and ignore Z for now.
- */
-Direction_full get_general_direction(Point origin, Point target);
-
-Direction_full get_general_direction(Tripoint origin, Point target);
-
-Direction_full get_general_direction(Point origin, Tripoint target);
-
-Direction_full get_general_direction(Tripoint origin, Tripoint target);
 
 // points_at_exact_distance() returns a list of all points exactly dist tiles
 // (using rl_dist() calculation) from origin.  Basically, a circle.
