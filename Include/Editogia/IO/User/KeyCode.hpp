ba671d2c5c4886a64aef949ec0e589b3b9816ed0
Joan Andrés <andres6936@live.com>
2021-06-04 13:14:49 -0500

    Created KeyCode enum

---
+++ Include/Editogia/IO/User/KeyCode.hpp
@@ -0,0 +1,102 @@
+// Joan Andrés (@Andres6936) Github.
+
+#ifndef EDITOGIA_KEYCODE_HPP
+#define EDITOGIA_KEYCODE_HPP
+
+namespace Editogia
+{
+
+	enum class KeyCode : short
+	{
+		NONE,
+		ESCAPE,
+		BACKSPACE,
+		TAB,
+		ENTER,
+		SHIFT,
+		CONTROL,
+		ALT,
+		PAUSE,
+		PAGE_UP,
+		PAGE_DOWN,
+		END,
+		HOME,
+		UP,
+		LEFT,
+		RIGHT,
+		DOWN,
+		PRINT_SCREEN,
+		DELETE,
+		SPACE,
+
+		A,
+		B,
+		C,
+		D,
+		E,
+		F,
+		G,
+		H,
+		I,
+		J,
+		K,
+		L,
+		M,
+		N,
+		O,
+		P,
+		Q,
+		R,
+		S,
+		T,
+		W,
+		X,
+		Y,
+		Z,
+
+		K_0,
+		K_1,
+		K_2,
+		K_3,
+		K_4,
+		K_5,
+		K_6,
+		K_7,
+		K_8,
+		K_9,
+
+		KP_0,
+		KP_1,
+		KP_2,
+		KP_3,
+		KP_4,
+		KP_5,
+		KP_6,
+		KP_7,
+		KP_8,
+		KP_9,
+
+		KP_ADD,
+		KP_SUB,
+		KP_DIV,
+		KP_MUL,
+		KP_DEC,
+		KP_ENTER,
+
+		F1,
+		F2,
+		F3,
+		F4,
+		F5,
+		F6,
+		F7,
+		F8,
+		F9,
+		F10,
+		F11,
+		F12,
+	};
+
+}
+
+#endif //EDITOGIA_KEYCODE_HPP
